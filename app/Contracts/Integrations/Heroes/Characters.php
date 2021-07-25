323d65842cd931872afa64d4eafdf6ac9aab40c1
PH7-Jack <pedrolivertwd@gmail.com>
2021-07-25 00:16:05 -0300

    feat: add fetch characters interface

---
+++ app/Contracts/Integrations/Heroes/Characters.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Contracts\Integrations\Heroes;
+
+use Illuminate\Support\Collection;
+
+interface Characters
+{
+    public const ORDER_BY_NAME = 'name';
+
+    /** @return Collection|Character[] */
+    public function get(): Collection;
+
+    public function getLimit(): int;
+
+    public function getPage(): int;
+
+    public function getOrderBy(): string;
+
+    public function getOrderDirection(): string;
+
+    public function getSearch(): ?string;
+
+    public function search(?string $search): self;
+
+    public function orderBy(string $orderBy): self;
+
+    public function orderDirection(string $orderDirection): self;
+
+    public function limit(int $limit): self;
+
+    public function page(int $page): self;
+}
