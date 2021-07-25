0af46bebe22cb9bc0a9c793d998c261c6744201b
PH7-Jack <pedrolivertwd@gmail.com>
2021-07-25 01:37:17 -0300

    feat: add listing heroes page

---
+++ app/Http/Livewire/Heroes/Index.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Livewire\Heroes;
+
+use App\Contracts\Integrations\Heroes\{Characters, Client};
+use App\Facades\HeroesClient;
+use Illuminate\Support\Collection;
+use Livewire\Component;
+
+/**
+ * @property-read Collection $characters
+ * @property-read array $orderDirections
+ */
+class Index extends Component
+{
+    public const ORDER_BY = Characters::ORDER_BY_NAME;
+
+    public string $orderDirection = Client::ORDER_ASC;
+
+    public ?string $search = null;
+
+    public int $page = 1;
+
+    public bool $loaded = false;
+
+    public function render()
+    {
+        return view('livewire.heroes.index');
+    }
+
+    public function getCharactersProperty(): Collection
+    {
+        return HeroesClient::characters()
+            ->orderBy(self::ORDER_BY)
+            ->orderDirection($this->orderDirection)
+            ->search($this->search)
+            ->page($this->page)
+            ->get();
+    }
+
+    public function getOrderDirectionsProperty(): array
+    {
+        return [
+            Client::ORDER_ASC  => __('Ascending Order'),
+            Client::ORDER_DESC => __('Descending Order'),
+        ];
+    }
+}
---
+++ resources/views/livewire/heroes/index.blade.php
@@ -0,0 +1,84 @@
+<div wire:init="$set('loaded', true)">
+    <div class="relative flex items-top justify-center min-h-screen sm:items-center py-4 sm:pt-0">
+        <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+            <div class="flex justify-center pt-4">
+                <x-logo class="h-20 rounded-lg shadow-md" />
+            </div>
+
+            <div class="flex items-center justify-between px-2 mt-8">
+                <div class="overflow-hidden rounded-full shadow mr-3">
+                    <x-input
+                        class="h-10 pl-10"
+                        wire:model.debounce.750ms="search"
+                        icon="search"
+                        borderless
+                        shadowless
+                    />
+                </div>
+
+                <x-dropdown>
+                    <x-slot name="trigger">
+                        <x-button class="mr-1" primary flat rounded>
+                            <x-icon name="filter" class="w-5 h-5" />
+                        </x-button>
+                    </x-slot>
+
+                    @foreach ($this->orderDirections as $direction => $label)
+                        <x-dropdown.item
+                            wire:key="directions.{{ $direction }}"
+                            wire:click="$set('orderDirection', '{{ $direction }}')"
+                            :label="$label"
+                        />
+                    @endforeach
+                </x-dropdown>
+            </div>
+
+            <div class="mt-4 grid grid-cols-2 gap-3 p-2">
+                @for ($i = 0; $i < 6; $i++)
+                    <div class="animate-pulse relative w-full h-48 bg-white shadow-soft border border-gray-100 rounded-lg"
+                        wire:key="loading.{{ $i }}"
+                        wire:loading>
+                        <div class="absolute inset-0 flex items-center justify-center">
+                            <x-icons.spinner class="w-6 h-6 text-gray-200" />
+                        </div>
+                    </div>
+                @endfor
+
+                @if ($loaded)
+                    @forelse ($this->characters as $character)
+                        <x-button
+                            class="!p-0 text-left"
+                            wire:key="characters.{{ $character->getId() }}"
+                            flat
+                            primary>
+                            <div class="overflow-hidden w-full h-full bg-white shadow-soft border border-gray-100 rounded-lg">
+                                <img
+                                    class="object-fill h-40 w-full"
+                                    src="{{ $character->getThumbnail() }}"
+                                    alt="{{ $character->getName() }}"
+                                    title="{{ $character->getName() }}"
+                                />
+
+                                <div class="p-2 flex flex-col">
+                                    <h3 class="truncate text-md text-gray-700">
+                                        {{ $character->getName() }}
+                                    </h3>
+
+                                    <span class="text-2xs">Category</span>
+                                </div>
+                            </div>
+                        </x-button>
+                    @empty
+                        <div class="col-span-2 text-center pt-16 pb-8 text-primary-400">
+                            <x-icons.spider class="w-32 h-32 mx-auto" />
+
+                            <p class="text-xs">
+                                @lang('Empty Characters')
+                            </p>
+                        </div>
+                    @endforelse
+                @endif
+            </div>
+        </div>
+    </div>
+</div>
